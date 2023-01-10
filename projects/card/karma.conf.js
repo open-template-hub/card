dc1c50e3130f5bbba1bf6cf1920ce5355cc758be
Furkan Yavuz <furknyavuz@gmail.com>
2023-01-10 03:32:31 +0300

    Update dependencies

--- projects/card/karma.conf.js
+++ projects/card/karma.conf.js
@@ -1,16 +1,16 @@
 // Karma configuration file, see link for more information
 // https://karma-runner.github.io/1.0/config/configuration-file.html
 
-module.exports = function (config) {
-  config.set({
-    basePath: '',
-    frameworks: ['jasmine', '@angular-devkit/build-angular'],
+module.exports = function ( config ) {
+  config.set( {
+    basePath: "",
+    frameworks: [ "jasmine", "@angular-devkit/build-angular" ],
     plugins: [
-      require('karma-jasmine'),
-      require('karma-chrome-launcher'),
-      require('karma-jasmine-html-reporter'),
-      require('karma-coverage'),
-      require('@angular-devkit/build-angular/plugins/karma')
+      require( "karma-jasmine" ),
+      require( "karma-chrome-launcher" ),
+      require( "karma-jasmine-html-reporter" ),
+      require( "karma-coverage" ),
+      require( "@angular-devkit/build-angular/plugins/karma" )
     ],
     client: {
       jasmine: {
@@ -25,20 +25,20 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, '../../coverage/card'),
-      subdir: '.',
+      dir: require( "path" ).join( __dirname, "../../coverage/card" ),
+      subdir: ".",
       reporters: [
-        { type: 'html' },
-        { type: 'text-summary' }
+        { type: "html" },
+        { type: "text-summary" }
       ]
     },
-    reporters: ['progress', 'kjhtml'],
+    reporters: [ "progress", "kjhtml" ],
     port: 9876,
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: true,
-    browsers: ['Chrome'],
+    browsers: [ "Chrome" ],
     singleRun: false,
     restartOnFileChange: true
-  });
+  } );
 };
